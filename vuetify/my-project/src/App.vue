e384b811bfe4cfa472a56e7f9c08f5a588047ae0
苦逼滴灰太狼 <346412931@qq.com>
2018-11-08 09:07:45 +0800

    add

--- vuetify/my-project/src/App.vue
+++ vuetify/my-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <router-view></router-view>
+    <router-view>hahah</router-view>
   </v-app>
 </template>
 
--- vuetify/my-project/src/page/index/index.vue
+++ vuetify/my-project/src/page/index/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        adsa 
+        peater
     </div>
 </template>
